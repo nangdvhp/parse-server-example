7f26e9b801671cf089e28db891820542a92d5f68
nangdvhp <nang@epi.com.vn>
2016-04-26 18:26:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,10 +12,24 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://dbparse:dbparse@ds011251.mlab.com:11251/dbparse',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID || 'myappid',
+  masterKey: process.env.MASTER_KEY || 'mynewmasterkey', //Add your master key here. Keep it secret!
+  
+      push: {
+      android: {
+        senderId: '717084254965',
+        apiKey: 'AIzaSyBzCH-N5NhqG3equXwuEXnsJfNKqreyGsM'
+      },
+      ios: {
+        pfx: '/file/path/to/XXX.p12',
+        bundleId: '',
+        production: false
+      }
+    }
+  
+  
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
