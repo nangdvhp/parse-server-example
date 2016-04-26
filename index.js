06e650caa55d7cb612ae21e498fe36eeffe49a06
nangdvhp <nang@epi.com.vn>
2016-04-27 00:49:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ var api = new ParseServer({
   appId: process.env.APP_ID || 'myappid',
   masterKey: process.env.MASTER_KEY || 'mynewmasterkey', //Add your master key here. Keep it secret!
   
-      push: {
-      android: {
-        senderId: '717084254965',
-        apiKey: 'AIzaSyBzCH-N5NhqG3equXwuEXnsJfNKqreyGsM'
-      }
+      // push: {
+      // android: {
+      //   senderId: '717084254965',
+      //   apiKey: 'AIzaSyBzCH-N5NhqG3equXwuEXnsJfNKqreyGsM'
+      // }
       // ,
       // ios: {
       //   pfx: '/file/path/to/XXX.p12',
